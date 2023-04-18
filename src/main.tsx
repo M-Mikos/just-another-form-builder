0a8cec40c9627a870eafc99a103f18ebb802fad3
M-Mikos <mikos.marcin.m@gmail.com>
2023-04-18 15:59:21 +0200

    Add routes and form types

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// React
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Router
+import { RouterProvider } from "react-router-dom";
+
+// Routes
+import routes from "./routes";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={routes} />
+);
---
+++ src/pages/Forms.tsx
@@ -0,0 +1,12 @@
+import FormList from "../components/FormsList";
+
+const Forms = () => {
+  return (
+    <>
+      <h2>Forms</h2>
+      <FormList />
+    </>
+  );
+};
+
+export default Forms;

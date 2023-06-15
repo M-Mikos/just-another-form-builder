77915798cf7df589e7ec377ad7dfe39aff93f144
M-Mikos <mikos.marcin.m@gmail.com>
2023-06-15 12:50:01 +0200

    Add proper data format for answers visualization

--- src/components/FormAnswers/AnswersSetDisplay.tsx
+++ src/components/FormAnswers/AnswersSetDisplay.tsx
@@ -2,7 +2,7 @@
 import { AnswerValueType } from "../../types/types";
 
 const AnswersSetDisplay = (props: { answers: string[] }): JSX.Element => {
-  const tryParseData = (answer: AnswerValueType) => {
+  const tryParseData = (answer: AnswerValueType): string[] | string => {
     if (answer.charAt(0) === "[") {
       return JSON.parse(answer);
     }
@@ -10,21 +10,48 @@ const AnswersSetDisplay = (props: { answers: string[] }): JSX.Element => {
   };
 
   const answersList = props.answers
-    .map((answer) => tryParseData(answer))
+    .map((answer: string) => tryParseData(answer))
     .flat();
 
-  const countedAnswers = answersList.reduce((sum, answer) => {
-    console.log("reducer:", sum);
-    const answers;
-    const answers = answers[answer] ? answers[answer]++ : (answers[answer] = 1);
-    return answers;
-  }, {});
+  const countedAnswers: { [key: string]: number } = answersList.reduce(
+    (sum: { [key: string]: number }, answer: string) => {
+      sum[answer] ? sum[answer]++ : (sum[answer] = 1);
+      return sum;
+    },
+    {}
+  );
 
-  console.log(answersList, countedAnswers);
+  const isAnswerRepetition: boolean =
+    Object.values(countedAnswers).filter((el) => el > 1).length !== 0;
+
+  const answersListElement: JSX.Element = (
+    <>
+      {Object.keys(countedAnswers).map((answer) => (
+        <p key={answer}>{answer}</p>
+      ))}
+    </>
+  );
+
+  const answersBarChartElement = (countedAnswers: {
+    [key: string]: number;
+  }): JSX.Element => {
+    return (
+      <>
+        {Object.keys(countedAnswers).map((answer) => (
+          <p key={answer}>
+            <span>{answer}: </span>
+            <span>{countedAnswers[answer]}</span>
+          </p>
+        ))}
+      </>
+    );
+  };
 
   return (
     <>
-      <p>{props.answers}</p>
+      {isAnswerRepetition
+        ? answersBarChartElement(countedAnswers)
+        : answersListElement}
     </>
   );
 };
